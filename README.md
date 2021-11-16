156aaa9a8c515580cca420932c45d3e1ce92a37d
Antonio Dottori <aadottori@gmail.com>
2021-11-15 22:52:10 -0300

    REST API; first endpoint; user schema; mongo db

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # node_express_api
+
+This is a CRUD API implementation using Node and Express.install
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+require("./controllers/authController")(app);
+
+
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`))

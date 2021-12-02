9df531e72cebfb14ee535c87d119d2ef7c501350
Antonio Dottori <aadottori@gmail.com>
2021-12-01 23:11:44 -0300

    Middleware and Token Validation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const PORT = 4000;
 app.use(express.json());
 
 require("./controllers/authController")(app);
+require("./controllers/projectController")(app);
 
 
 app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`))
